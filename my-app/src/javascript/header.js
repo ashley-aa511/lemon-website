53cf73de9f86b79f298428411985c62a3449f4f5
ashley <okeyoashley@gmail.com>
2023-08-09 22:49:42 +0300

    changes

--- my-app/src/javascript/header.js
+++ my-app/src/javascript/header.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
+
 
 function Header() {
   return (
-  <header>
-    <img src="" alt="LITTLE LEMON/>"
-  </header>
+  <head>  
+    <Helmet>
+      <meta name="description" content="A delightful dining experience at Little Lemon restaurant. Explore our delicious menu and enjoy a blend of flavors that will tantalize your taste buds." />
+      <meta name="og:title" content="LITTLE LEMON RESTAURANT" />
+      <meta name="og:image" content=""/>
+      </Helmet>
+        <img src="" alt="LITTLE LEMON/>"
+  </head>
+  
   );
 }
 
