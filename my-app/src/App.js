09480af745ea3bed33e9dc40fcf6c1b3e844115c
ashley <okeyoashley@gmail.com>
2023-08-09 21:40:01 +0300

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ my-app/src/javascript/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+  <header>
+    <img src="" alt="LITTLE LEMON/>"
+  </header>
+  );
+}
+
+export default Header;
